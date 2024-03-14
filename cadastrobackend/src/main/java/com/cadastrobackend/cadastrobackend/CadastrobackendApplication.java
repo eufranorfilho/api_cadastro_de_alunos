98a3a25065b70f0cd516609e22bcb981f2dce687
eufranorfilho <eufranorfilho.dev@gmail.com>
2024-03-14 16:19:05 -0300

    first commit

---
+++ cadastrobackend/src/main/java/com/cadastrobackend/cadastrobackend/CadastrobackendApplication.java
@@ -0,0 +1,13 @@
+package com.cadastrobackend.cadastrobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrobackendApplication.class, args);
+	}
+
+}
---
+++ cadastrobackend/src/test/java/com/cadastrobackend/cadastrobackend/CadastrobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadastrobackend.cadastrobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
